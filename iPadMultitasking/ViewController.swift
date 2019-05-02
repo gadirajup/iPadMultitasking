d02c1660f9079ca6c9503a26029f43f0f2d790c9
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-01 19:54:19 -0700

    add multiple webviews

--- iPadMultitasking/ViewController.swift
+++ iPadMultitasking/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var urlBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Multibrowser"
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWebView))
+        
+        let deleteButton = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(removeWebView))
+        
+        navigationItem.rightBarButtonItems = [addButton, deleteButton]
     }
 
+    @objc func addWebView() {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        stackView.addArrangedSubview(webView)
+        
+        webView.load(URLRequest(url: URL(string: "https://www.google.com")!))
+    }
+    
+    @objc func removeWebView() {
+        
+    }
 
 }
 
