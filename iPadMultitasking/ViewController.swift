307fdfbb3af1cd365e789295d29c60f4813d35db
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-01 20:29:11 -0700

    Delete WebViews

--- iPadMultitasking/ViewController.swift
+++ iPadMultitasking/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate {
+    
+    weak var activeWebView: WKWebView?
 
     @IBOutlet weak var urlBar: UITextField!
     @IBOutlet weak var stackView: UIStackView!
@@ -29,15 +31,74 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
     @objc func addWebView() {
         let webView = WKWebView()
         webView.navigationDelegate = self
+        webView.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).cgColor
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(webViewTapped))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
         
         stackView.addArrangedSubview(webView)
         
         webView.load(URLRequest(url: URL(string: "https://www.google.com")!))
+        
+        select(webView)
+        activeWebView = webView
+    }
+    
+    func select(_ webView: WKWebView) {
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+        
+        activeWebView = webView
+        webView.layer.borderWidth = 3
+        
+        navigationItem.title = webView.title
     }
     
     @objc func removeWebView() {
+        guard let webView = activeWebView else { return }
+        if let index = stackView.arrangedSubviews.firstIndex(of: webView) {
+            stackView.removeArrangedSubview(webView)
+            webView.removeFromSuperview()
+            
+            if stackView.arrangedSubviews.count == 0 {
+                title = "MultiBrowser"
+            } else {
+                let newViewIndex = index.advanced(by: -1)
+                if let newView = stackView.arrangedSubviews[newViewIndex] as? WKWebView {
+                    activeWebView = newView
+                    select(newView)
+                }
+            }
+        }
+    }
+    
+    @objc func webViewTapped(tap: UITapGestureRecognizer) {
+        if let view = tap.view as? WKWebView {
+            select(view)
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
+        guard textField.hasText else { return true }
+        guard let text = textField.text else { return true }
+        guard let webView = activeWebView else { return true }
+        guard let url = URL(string: text) else { return  true }
+        
+        webView.load(URLRequest(url: url))
+        
+        textField.resignFirstResponder()
+        return true
     }
 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
